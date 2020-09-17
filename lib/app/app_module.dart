c59139a969f289518cfb89d59f8e1de1c2de8589
Serveloja <serveloja@Mac-mini-de-Charles.local>
2020-09-17 17:38:06 -0300

    home module, spalsh screen

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,20 +1,37 @@
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_app/app/pages/splash/splash_page.dart';
+
+import 'pages/splash/splash_controller.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter_app/app/app_controller.dart';
-import 'package:flutter_app/app/app_widget.dart';
+import 'package:flutter_app/app/modules/home/home_controller.dart';
+import 'package:flutter_app/app/modules/other/other_page.dart';
+import 'package:flutter_app/app/shared/utils/constants.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_app/app/app_widget.dart';
+import 'package:flutter_app/app/modules/home/home_module.dart';
+
+import 'modules/home/repositories/poke_repository.dart';
 
 class AppModule extends MainModule {
   @override
-  // TODO: implement binds
   List<Bind> get binds => [
+        $SplashController,
         Bind((i) => AppController()),
+        Bind((i) => HomeController(i.get<PokeRepository>())),
+        Bind((i) => PokeRepository(i.get<Dio>())),
+        Bind((i) => Dio(BaseOptions(baseUrl: URL_BASE)))
       ];
 
   @override
-  // TODO: implement routers
-  List<ModularRouter> get routers => [];
+  List<ModularRouter> get routers => [
+        ModularRouter(Modular.initialRoute, child: (_, args) => SplashPage()),
+        ModularRouter('/home', module: HomeModule()),
+        ModularRouter('/routeOther', child: (context, args) => OtherPage()),
+      ];
 
   @override
-  // TODO: implement bootstrap
   Widget get bootstrap => AppWidget();
+
+  static Inject get to => Inject<AppModule>.of();
 }
--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -2,15 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class AppWidget extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      navigatorKey: Modular.navigatorKey,
+      title: 'Flutter Slidy',
       theme: ThemeData(
         primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       initialRoute: '/',
       onGenerateRoute: Modular.generateRoute,
